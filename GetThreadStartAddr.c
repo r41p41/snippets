78084f15d44bc9d76a0eb27e5660fd4f4c94cb07
r41p41 <kaelsunstrider.raghav@gmail.com>
2014-02-02 06:04:25 +0530

    Grab Thread's True OEP
    Undocumented Method

--- GetThreadStartAddr.c
+++ GetThreadStartAddr.c
@@ -15,43 +15,37 @@ DWORD GetThreadStartAddress(HANDLE hThread)
 		DWORD a,b,c,d,e;
 	}THREAD_BASIC_INFORMATION;
 
-	THREAD_BASIC_IFNORMATION tbi;
+	THREAD_BASIC_INFORMATION tbi;
 	FARPROC qti;
 	DWORD ntdll,temp=0,temp2=0,temp3=0,pd=0;
 	ntdll = (DWORD) GetModuleHandle("ntdll");
-	SuspendThread(hThread);
 	if(ntdll)
 	{
 		qti=GetProcAddress(ntdll,"ZwQueryInformationThread");
 		if(qti)
 		{
 			qti(hThread,0,&tbi,sizeof(THREAD_BASIC_INFORMATION),NULL);
-			if(tbi)
+			if(tbi.TebBaseAddress)
 			{
-				ReadProcessMemory(hThread,tbi.TebBaseAddress,&temp2,4,&pd); //temp2 gets fs:[0x00] or current seh handler
-				if(pd==4)
+				SuspendThread(hThread);
+				
+				if(ReadProcessMemory(hProcess,tbi.TebBaseAddress,&temp2,4,&pd)) 	//temp2 gets fs:[0x00] or current seh handler)
 				{
-					while(temp!=0xffffffff)             					//iterate all seh handlers till we reach last
+					while(temp!=0xffffffff)             							//iterate all seh handlers till we reach last
 					{
-                    	ReadProcessMemory(open,temp2,&temp,4,&pd);			
-						if(pd!=4)
+						if(ReadProcessMemory(hProcess,temp2,&temp,4,&pd)==0)
 						break;
                     	if(temp!=0xffffffff)temp2=temp;
                     }
 				}
+				ResumeThread(hThread);
 			}
 		}
 	}
-	if(pd!=4) //didn't work out
-	temp2=0;
-	else
-	{
-		temp=0;
-		temp3=0;
-		ReadProcessMemory(open,temp2+0x18,&temp,4,&pd);
-		ReadProcessMemory(open,temp2+0x30,&temp3,4,&pd);
-	}
-	ResumeThread(hThread);
+	temp=0;
+	temp3=0;
+	ReadProcessMemory(open,temp2+0x18,&temp,4,&pd);
+	ReadProcessMemory(open,temp2+0x30,&temp3,4,&pd);
 	/*  experimental
 	if(temp == temp3) 	
 	return temp;		
